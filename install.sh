762c42393fbbfa816d7516407864e3234512e533
hymal <hymalm01052006@gmail.com>
2026-02-21 13:23:42 +0000

    Initial commit: added bashrc, tmux.conf and install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Створюємо посилання на наш конфіг
 ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 echo "Dotfiles installed! Restart your terminal."
